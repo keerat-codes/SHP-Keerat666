8fd6458be7821841028998f3579810b3d828043a
Gurkeerat Singh <gurkeeratsondhi@gmail.com>
2023-08-07 20:33:42 +0530

    Updated App.js for hr route using AutoPilot

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ require('dotenv').config();
 app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+const hrRoute = require('./routes/hr');
+app.use('/hr', hrRoute);
 
 const conn = require('./middlewares/connection');
 console.log("Trying to establish a connection to TiDB...");
