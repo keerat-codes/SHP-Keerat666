16495afb6ac562ab86270e73b81c160844d3837a
Gurkeerat Singh <gurkeeratsondhi@gmail.com>
2023-08-07 20:35:06 +0530

    Updated App.js for campaign route using AutoPilot

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ require('dotenv').config();
 app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+const campaignRoute = require('./routes/campaign');
+app.use('/campaign', campaignRoute);
 const hrRoute = require('./routes/hr');
 app.use('/hr', hrRoute);
 
