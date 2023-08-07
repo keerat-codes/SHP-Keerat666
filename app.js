846ae5650a3d8f11cfbc2c85643ae96983838654
Gurkeerat Singh <gurkeeratsondhi@gmail.com>
2023-08-07 20:40:52 +0530

    Updated app.js using AutoPilot

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ require('dotenv').config();
 app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+const email  = require('./controllers/email');
+app.use('/controller', email);
 const testAttemptsRoute = require('./routes/testAttempts');
 app.use('/testAttempts', testAttemptsRoute);
 const screeningTestRoute = require('./routes/screeningTest');
