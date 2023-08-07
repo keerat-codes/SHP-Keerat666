77f170838759c884ba4ecfb1d5dff7a60dd3b4b0
Gurkeerat Singh <gurkeeratsondhi@gmail.com>
2023-08-07 20:36:33 +0530

    Updated App.js for screeningTest route using AutoPilot

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ require('dotenv').config();
 app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+const screeningTestRoute = require('./routes/screeningTest');
+app.use('/screeningTest', screeningTestRoute);
 const campaignRoute = require('./routes/campaign');
 app.use('/campaign', campaignRoute);
 const hrRoute = require('./routes/hr');
